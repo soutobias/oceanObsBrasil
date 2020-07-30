b324f12dfc0abad49a3a86b502761d6e541eb67e
Felippe Galdino <ocfgaldino@gmail.com>
2020-07-30 14:27:41 -0300

    Insertion of sst scripts

---
+++ mur/mur_download.py
@@ -0,0 +1,179 @@
+
+import os
+import time
+
+from datetime import date, datetime, timedelta
+
+
+import xarray as xr
+import pandas as pd
+import numpy as np
+# Pontos de coleta:
+
+import sys
+import os
+from os.path import expanduser
+home = expanduser("~")
+sys.path.insert(0,home)
+import user_config
+os.chdir( user_config.path )
+
+from databaseMySQL import consulta_data_banco, insere_dado_banco, deleta_dado
+
+
+
+# Last date of data to start the download
+start_date = consulta_data_banco('mur')
+
+
+date_format = "%Y%m%d"
+
+
+if start_date == None:
+    start_date = (date.today() - timedelta(days = 5)).strftime(date_format)
+else:
+    start_date = start_date.strftime(date_format)
+
+end_date = date.today().strftime(date_format)
+
+
+
+
+pontos = pd.read_csv('Pontos MUR - CPTEC.csv',sep=',')
+
+pontos_num = pontos['Ponto']
+pontos_lat = pontos['Lat']
+pontos_lon = pontos['Lon']
+
+# Transformando lons 0 - 360 para -180 +180
+pontos_lon = ((pontos_lon + 180) % 360 - 180)
+pontos_lon = pontos_lon.round(2)
+pontos_lat = pontos_lat.round(2)
+
+
+
+range_coord = 0.00
+
+
+
+for p in range(len(pontos_lat)):
+    lat_coleta = pontos_lat[p]
+    lon_coleta = pontos_lon[p]
+
+    # lon_min lon_max lat_min lat_max
+
+    box_coord = [str(lon_coleta -range_coord),\
+     str(lon_coleta + range_coord), \
+     str(lat_coleta - range_coord),\
+     str(lat_coleta + range_coord)]
+
+
+    str_coord = ' '.join(box_coord)
+
+
+    str_download = 'python3.8 mur/subset_mur.py -s %s -f %s -b %s -x MUR-JPL-L4-GLOB-v4.1' % (start_date, end_date,str_coord)
+
+    download_status = os.system(str_download)
+
+    print('Download - Ponto %s OK!' % str(pontos_num[p]))
+
+
+
+
+# Loading data into a xarray
+data = xr.open_mfdataset('*.nc', combine = 'by_coords', concat_dim = 'ponto')
+
+
+dados_df = data.to_dataframe()
+
+
+###
+# Delete Files Downloaded
+
+list_all_files = os.listdir('.')
+
+list_files = [file for file in list_all_files if file.endswith("subset_MUR.nc")]
+
+
+if list_files:
+    for file in list_files:
+        os.remove(file)
+
+###
+
+
+
+#
+# len(dados_df)
+#
+sst = dados_df['analysed_sst']
+sst = pd.DataFrame(sst.loc[pd.notnull(sst)])
+
+# Transformando Index Para Colunas
+sst.reset_index(inplace = True)
+
+
+
+# Formatando valores :
+sst['lat'] = sst['lat'].round(2)
+sst['lon'] = sst['lon'].round(2)
+
+
+# SST Kelvin to Celsius
+sst['analysed_sst'] = (sst['analysed_sst'] - 273.15).round(2)
+
+# Rename Columns
+sst = sst.rename(columns = {'analysed_sst':'sst', 'time':'datahora'})
+
+
+
+# Coletando valores nas localizações exatas dos pontos requisitados:
+
+sst_final = pd.DataFrame()
+
+for p in range(len(pontos_lat)):
+    lat_coleta = pontos_lat[p]
+    lon_coleta = pontos_lon[p]
+
+    sst_value = sst[(sst['lat'] == lat_coleta) & (sst['lon'] == lon_coleta)]
+
+
+    sst_final = sst_final.append(sst_value)
+
+
+
+sst_final.reset_index(drop= True, inplace=True)
+
+
+
+
+# Data Formatting
+
+
+
+
+sst_final['lat'] = sst_final['lat'].astype(np.float64)
+sst_final['lon'] = sst_final['lon'].astype(np.float64)
+sst_final['sst'] = sst_final['sst'].astype(np.float64)
+sst_final['datahora'] = sst_final['datahora'].apply(lambda data: datetime.strftime(data,"%Y-%m-%d %H:%M:%S"))
+
+print(sst_final)
+
+
+
+# Deleting existing data // avoid duplicate
+
+min_date = min(sst_final['datahora'])
+
+condition = "WHERE datahora >= '%s'" % min_date
+
+deleta_dado('mur', condition)
+
+insere_dado_banco('mur', sst_final)
+
+
+
+
+
+
+print('Programa MUR finalizado.')
---
+++ mur/plot_tsm.py
@@ -0,0 +1,232 @@
+
+
+import sys
+import os
+from os.path import expanduser
+home = expanduser("~")
+
+
+import pandas as pd
+
+
+
+### DataViz Packages
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import cartopy.feature as cfeature
+from cartopy import config
+from shapely import geometry
+
+
+import os
+from os.path import expanduser
+
+
+# Builded Packages
+sys.path.insert(1,'/home/ocf/Marinha/Projetos/Remobs/remobs/database')
+from databaseMySQL import consulta_dados
+
+
+
+# Dataframe from database
+
+dados_sst = consulta_dados('mur', 'SELECT * FROM mur WHERE datahora = (SELECT max(datahora) FROM mur)')
+
+
+
+# MetaAreas :
+
+lines = open('/home/ocf/Marinha/Projetos/Remobs/remobs/metareav.txt', 'r')
+
+lat1,lat2,lat3,lat4,lat5,lat6,lat7,lat8,lat9,lat10=[],[],[],[],[],[],[],[],[],[]
+lon1,lon2,lon3,lon4,lon5,lon6,lon7,lon8,lon9,lon10=[],[],[],[],[],[],[],[],[],[]
+i=1
+for line in lines:
+    dataline = line.strip()
+    columns = dataline.split()
+    if columns[0]!='0':
+        exec("lat%s.append(float(columns[1]))"%str(i))
+        exec("lon%s.append(float(columns[2]))"%str(i))
+    else:
+        i=i+1
+
+
+
+
+lons = [lon1, lon2, lon3, lon4, lon5, lon6, lon7, lon8, lon9, lon10]
+lats = [lat1, lat2, lat3, lat4, lat5, lat6, lat7, lat8, lat9, lat10]
+
+
+
+# Limites das áreas para plot
+
+# Area Alfa
+lat_a_1=-37
+lat_a_2=-27
+lon_a_1=-55
+lon_a_2=-42
+
+
+# Area Bravo
+lat_b_1=-32
+lat_b_2=-22
+lon_b_1=-49
+lon_b_2=-37
+
+# Area Charlie
+lat_c_1=-29
+lat_c_2=-22
+lon_c_1=-49
+lon_c_2=-42
+
+# Area Delta
+lat_d_1=-27
+lat_d_2=-16
+lon_d_1=-42
+lon_d_2=-32
+
+# Area ECHO
+lat_e_1=-22
+lat_e_2=-12
+lon_e_1=-40
+lon_e_2=-32
+
+
+# Area FOXTROT
+lat_f_1=-16
+lat_f_2=-3
+lon_f_1=-39
+lon_f_2=-25
+
+
+# Area GOLF
+lat_g_1 = -6
+lat_g_2= 2
+lon_g_1=-45
+lon_g_2=-28
+
+
+# Area HOTEL
+lat_h_1=-37
+lat_h_2=-27
+lon_h_1=-55
+lon_h_2=-42
+
+
+# Box Coords das Metareas
+# ex: lat_x_1 = min_coord , lat_x_2 = max_coord
+
+
+
+metareas = pd.DataFrame({'Area':['Alfa','Bravo','Charlie', 'Delta', 'Echo', 'Foxtrot', 'Golf'],
+                            'Lat_1': [lat_a_1, lat_b_1, lat_c_1, lat_d_1, lat_e_1, lat_f_1,lat_g_1],
+                            'Lat_2': [lat_a_2, lat_b_2, lat_c_2, lat_d_2, lat_e_2, lat_f_2,lat_g_2],
+                            'Lon_1': [lon_a_1, lon_b_1, lon_c_1, lon_d_1, lon_e_1, lon_f_1,lon_g_1],
+                            'Lon_2': [lon_a_2, lon_b_2, lon_c_2, lon_d_2, lon_e_2, lon_f_2,lon_g_2]})
+
+
+
+
+
+
+#
+#
+# # PLOT TODAS ZONAS
+# fig = plt.figure(figsize=(10,10))
+# i = 0
+# for area in range(len(metareas)):
+#     if metareas['Area'][area] == 'Bravo':
+#         continue
+#     else:
+#         i += 1
+#         ax = fig.add_subplot(2,3,i,projection=ccrs.PlateCarree())
+#         ax.add_feature(cfeature.LAND)
+#         ax.add_feature(cfeature.COASTLINE)
+#         # ax.add_feature(states_provinces, edgecolor='gray')
+#         # ax.add_feature(cfeature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='face', facecolor='0.8'))
+#         ax.set_xlim(metareas['Lon_1'][area] , metareas['Lon_2'][area])
+#         ax.set_ylim(metareas['Lat_1'][area] , metareas['Lat_2'][area])
+#
+#
+#         # Colocando a zona
+#         gr = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+#                      linewidth=0.5, color='gray', alpha=0.6, linestyle='--', fontsize = 6)
+#
+#
+#
+#
+
+
+
+######### SUBPLOTS ZONAS ################################
+
+# String details :
+# date:
+data_dados = max(dados_sst['datahora'])
+data_plot = data_dados.strftime("%Y-%m-%d")
+
+
+
+fig = plt.figure(figsize=(8,6))
+i = 0
+for area in range(len(metareas)):
+    if metareas['Area'][area] == 'Bravo':
+        continue
+    else:
+        i += 1
+        ax = fig.add_subplot(2,3,i,projection=ccrs.PlateCarree())
+        ax.add_feature(cfeature.LAND)
+        ax.add_feature(cfeature.COASTLINE)
+        # ax.add_feature(states_provinces, edgecolor='gray')
+        # ax.add_feature(cfeature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='face', facecolor='0.8'))
+        ax.set_xlim(metareas['Lon_1'][area] , metareas['Lon_2'][area])
+        ax.set_ylim(metareas['Lat_1'][area] , metareas['Lat_2'][area])
+
+
+        # Colocando a zona
+        gr = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                     linewidth=0.5, color='gray', alpha=0.6, linestyle='--')
+
+        gr.top_labels = False
+        gr.right_labels = False
+        gr.xlabel_style = {'size':6}
+        gr.ylabel_style = {'size':6}
+
+        # Lines zones
+
+        for zona in range(len(lats)):
+            ax.plot(lons[zona],lats[zona],linewidth=0.5,color='k')
+
+
+
+        ax.set_title("Área %s" % metareas['Area'][area])
+        ax.set_box_aspect(1)
+
+
+
+        # Colocando os ponto
+        ax.scatter(dados_sst['lon'],dados_sst['lat'], c = dados_sst['sst'], s = 1.6,cmap = 'Reds')
+
+
+        # Plotando temp em cada ponto :
+        ## Condicional em Golf para mudar a posição dos valores
+        for pos in range(len(dados_sst)):
+            if metareas['Area'][area] == 'Golf':
+                if dados_sst['lat'][pos] > metareas['Lat_1'][area] and dados_sst['lat'][pos] < metareas['Lat_2'][area]:
+                    ax.annotate(str(round(dados_sst['sst'][pos],1)),xy=(dados_sst['lon'][pos]+0.15,dados_sst['lat'][pos]+0.15),color='k',fontsize=4, fontweight = 'bold')
+            else:
+                if dados_sst['lat'][pos] > metareas['Lat_1'][area] and dados_sst['lat'][pos] < metareas['Lat_2'][area]:
+                    ax.annotate(str(round(dados_sst['sst'][pos],1)),xy=(dados_sst['lon'][pos]+0.15,dados_sst['lat'][pos]),color='k',fontsize=4, fontweight = 'bold')
+
+
+
+        # set a margin around the data
+
+
+        ax.margins(0.005,0.005)
+
+
+
+fig.subplots_adjust(top=0.9, bottom=0.08, hspace = 0.05)
+fig.suptitle("TSM - %s" % data_plot, size = 16, y = 0.95)
+plt.savefig("tsm_ultimo_dado.jpg", dpi = 300)
